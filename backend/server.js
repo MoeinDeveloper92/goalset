46d28613472023d491962bec363abae5a92f3f63
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-08-04 14:12:50 +0200

    some changes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Hello Moein")
+const express = require("express")
+const dotenv = require("dotenv").config()
+
+const PORT = process.env.PORT || 6000;
+
+
+const app = express()
+app.get("/api/goals", (req, res) => {
+    res.status(200).json({
+        message: "Hello Moein, you hit right endpoint"
+    })
+})
+app.listen(PORT, () => {
+    console.log(`Server started on PORT ${PORT}`)
+})
